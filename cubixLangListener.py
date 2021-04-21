ab48b1010029bca51d556fbea5b896cab97c74b4
Paradise-blaze <jan4000@o2.pl>
2021-04-21 19:11:49 +0200

    Some method implementing

--- cubixLangListener.py
+++ cubixLangListener.py
@@ -1,49 +1,56 @@
+from typing import Dict
 from cubixListener import cubixListener as CubixListener
 from init import initial_html, ending_html
 from antlr4 import *
 import sys
+from cubixParser import cubixParser
 
 
 class CubixLangListener(CubixListener):
+
 
     def __init__(self, output):
         self.output = output
         self.output.write(initial_html)
 
-    
-    def enterStart(self, ctx):
-        print(f"{ctx.getText()}\n")
+        self.variablesDictionary = {}
 
 
     def enterNumberInitalization(self, ctx):
-        print(f"Enter num: {ctx.getText()}\n")
+        name = ctx.VariableName().getText()
+        value = ctx.NUMBER().getText()
+        if(name in self.variablesDictionary.keys()):
+            print("Variable with this name already exist")
+        else:
+            self.variablesDictionary[name] = value
+        
 
-    # def exitNumberInitalization(self, ctx):
-    #     print(f"Exit num: {ctx.getText()}")
-
-
-    # def enterExpression(self, ctx):
-    #     print(f"Enter expr: {ctx.getText()}")
-
-    # def exitExpression(self, ctx):
-    #     print(f"Exit expr: {ctx.getText()}")
-
-
-    # def enterMoveInitalization(self, ctx):
-    #     print(f"Enter move: {ctx.getText()}")
-
-    # def exitMoveInitalization(self, ctx):
-    #     print(f"Exit move: {ctx.getText()}")
+    def enterMoveInitalization(self, ctx):
+        name = ctx.VariableName().getText()
+        value = ctx.MOVEVALUE().getText()
+        if(name in self.variablesDictionary.keys()):
+            print("Variable with this name already exist")
+        else:
+            if(value in self.variablesDictionary.keys()):
+                self.variablesDictionary[name] = self.variablesDictionary[value]
+            else:
+                self.variablesDictionary[name] = value
 
 
     def enterCubeInitialization(self, ctx):
+        name = ctx.VariableName().getText()
+        value = ctx.CubeValue().getText()
+        print(value)
+        if(name in self.variablesDictionary.keys()):
+            print("Variable with this name already exist")
+        else:
+            self.variablesDictionary[name] = value
+
+
         print(f"Enter cubeint: {ctx.getText()}")
         print(f"Type: {ctx.CUBE()}")
         print(f"Variable name: {ctx.VariableName()}")
         print(f"Value: {ctx.CubeValue()}\n")
-
-    # def exitCubeInitialization(self, ctx):
-    #     print(f"Exit cubeint: {ctx.getText()}")
 
 
     def exitStart(self, ctx):
